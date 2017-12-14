e9013d4a1e3e408b193ec621b1164445cc409a3a
Domination7 <31801630+Domination7@users.noreply.github.com>
2017-12-13 23:00:17 -0600

    Initially edited files

---
+++ index.js
@@ -0,0 +1,23 @@
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
+var express = require('express');
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(8080, function(){
+  console.log('Listening on port 8080')
+  console.log('Type localhost:8080 to join server')
+});
